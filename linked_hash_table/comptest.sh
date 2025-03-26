a6dff3010c4c1bbb4bab172877fcf8f3cdcf03be
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-03-25 23:50:34 -0500

    created skip_list

--- linked_hash_table/comptest.sh
+++ linked_hash_table/comptest.sh
@@ -1,9 +1,9 @@
 clear
 
-rm -f test
+rm -f test.out
 
 echo "\033[33m===== Compiling testcases.c =====\033[0m\n"
-gcc -Wall -o test testcases.c
+gcc -Wall -o test.out testcases.c
 
 if [[ "$?" != 0 ]]; then
 	echo
@@ -13,6 +13,6 @@ fi
 echo "\033[92mCompiled Successfully\033[0m\n"
 
 if [[ "$1" != "" ]]; then
-	echo "\033[33m===== Executing '\033[31m./test $1\033[33m' =====\033[0m\n"
-	./test $1
+	echo "\033[33m===== Executing '\033[31m./test.out $1\033[33m' =====\033[0m\n"
+	./test.out $1
 fi
